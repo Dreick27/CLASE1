159ac41c79e0fcbf2b48fc455d46fee8bd12f7c6
Dreick27 <janicejq304@gmail.com>
2024-08-10 14:01:44 -0500

    Clase

---
+++ Practica1/View/Vista1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Vista1 : ContentPage
+    {
+        public Vista1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Guardar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Vista2());
+        }
+    }
+}
